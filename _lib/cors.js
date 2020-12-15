530a22f901bbe37ee787111222039482ed88567a
John Philip Morgan <john@useproof.com>
2020-12-15 09:21:32 -0600

    Get CORS_ALLOW_ORIGIN from env

--- _lib/cors.js
+++ _lib/cors.js
@@ -1,6 +1,6 @@
 module.exports = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", `${process.env.CORS_ALLOW_ORIGIN ? process.env.CORS_ALLOW_ORIGIN : '*'}`);
   // another option
   // res.setHeader('Access-Control-Allow-Origin', req.header.origin);
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
